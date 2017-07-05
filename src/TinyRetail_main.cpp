dd6b32534b26339d263eddbba2554c7efd559ce5
tomohiro1226 <12.26karoshi@gmail.com>
2017-07-06 01:41:09 +0900

    一部変更

--- src/TinyRetail_main.cpp
+++ src/TinyRetail_main.cpp
@@ -5,5 +5,13 @@ int main(int argc,char *argv[]){
 
     TinyRetail_Julius *pTinyRetail = new TinyRetail_Julius();
 
+    int ret = pTinyRetail->Begin();
+    fprintf(stdout,"Begin() ret = %d",ret);
+
+    getchar();
+
+    ret = pTinyRetail->start_stream();
+    fprintf(stdout,"start_stream() ret = %d",ret);
+
     return 0;
 }
