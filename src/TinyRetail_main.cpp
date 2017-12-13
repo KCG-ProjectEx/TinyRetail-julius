250db5f15b4621807ebe4d463eb2d84b171797ac
Tomohiro <12.26tomo.aoki@gmail.com>
2017-12-13 16:32:20 +0900

    nega poji API試用運転

--- src/TinyRetail_main.cpp
+++ src/TinyRetail_main.cpp
@@ -1,10 +1,15 @@
 #include <TinyRetail_Julius.h>
-#include <Post_curl.h>
+#include <Curl.hpp>
 #include <JSON.h>
 // #include <negaPosi.h>
 
 #include <stdio.h>
+#include <iostream>
 #include <string>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/foreach.hpp>
+#include <boost/optional.hpp>
 
 using namespace std;
 
@@ -25,8 +30,13 @@ int main(int argc,char *argv[]){
     fprintf(stdout,"curl Begin() ret = %d\n",ret);
 
     // //==========negaPosi初期化==========
-    // CnegaPosi *pCnegaPosi = new CnegaPosi("./src/dat/pn.dic
-    // ");
+    CCurl *pCCurlNegaPoji = new CCurl();
+    ret = pCCurlNegaPoji->Begin("https://api.apitore.com/api/11/sentiment/predict");
+    fprintf(stdout,"curl negapoji Begin() ret = %d\n",ret);    
+    // tokenを追記
+    string reqDef += "access_token=c239e412-dafa-41b2-a2f8-e273abbc934f";
+    // 識別データの追記
+    reqDef += "&text=";
 
 
     //getchar();
@@ -64,11 +74,45 @@ int main(int argc,char *argv[]){
         // sscanf(tag_tmp.sentence.c_str(),"%d%s",&word_id,word_str);
         // fprintf(stdout,"word_str = %s\n",word_str);
 
-        // // 受け取ったデータをjson形式に変換
+/***************************** */
+
+        // 受け取ったデータをnega poji APIに投げる
+        string strReq = reqDef;
+        strReq += tag_tmp.sentence;
+
+        string res 
+        res = pCCurlNegaPoji->send_get(strReq);
+
+        // string型をstring stream型に変更
+        stringstream stmData;
+        stmData << res;
+
+        // jsonデータを得るために、ptreeに格納する
+        ptree pt;
+        read_json(stmData, pt);
+
+        // jsonデータの獲得
+        if (boost::optional<string> str = pt.get_optional<string>("predict.sentiment")) {
+            cout << "sentiment : " << str.get() << endl;
+            res = str.get();
+        }
+        else {
+            cout << "sentiment is nothing" << endl;
+        }
+        if (boost::optional<double> value = pt.get_optional<double>("predict.score")) {
+            cout << "score : " << value.get() << endl;
+        }
+        else {
+            cout << "score is nothing" << endl;
+        }
+
+/***************************** */
+
+        // 受け取ったデータをjson形式に変換
         CJSON *pCJSON = new CJSON();
         pCJSON->push("mic_id","1");
         pCJSON->push("sentence",tag_tmp.sentence);
-        pCJSON->push("favor","pojinega");
+        pCJSON->push("favor",res);
         // pCJSON->push("word_id",word_id+53);
         // pCJSON->push("word_rbd",word_rbd);
 
@@ -77,7 +121,7 @@ int main(int argc,char *argv[]){
 
         #if 1
         // postで送信する
-        if((pCCurl->send_post(pCJSON->pop())) == -1){ //異常終了したら
+        if((pCCurl->send_post(pCJSON->pop())) == ""){ //異常終了したら
             fprintf(stderr,"ERR : not send to post\n");
             break;
         }
